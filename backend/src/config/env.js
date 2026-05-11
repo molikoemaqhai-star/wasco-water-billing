a4b514d06d2daf59fd15d1d742f301d468a34ec2
molikoemaqhai-star <molikoemaqhai@gmail.com>
2026-05-11 09:51:50 +0200

    Fix Railway environment variables

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -3,18 +3,20 @@ dotenv.config();
 
 export const env = {
   port: Number(process.env.PORT || 4000),
+
   pg: {
-    host: process.env.PG_HOST || "localhost",
-    port: Number(process.env.PG_PORT || 5432),
-    database: process.env.PG_DATABASE || "wasco_water_billing",
-    user: process.env.PG_USER || "postgres",
-    password: process.env.PG_PASSWORD || "12345"
+    host: process.env.PG_HOST,
+    port: Number(process.env.PG_PORT),
+    database: process.env.PG_DATABASE,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD
   },
+
   mysql: {
-    host: process.env.MYSQL_HOST || "localhost",
-    port: Number(process.env.MYSQL_PORT || 3306),
-    database: process.env.MYSQL_DATABASE || "wasco_water_billing",
-    user: process.env.MYSQL_USER || "root",
-    password: process.env.MYSQL_PASSWORD || "123456"
+    host: process.env.MYSQL_HOST,
+    port: Number(process.env.MYSQL_PORT),
+    database: process.env.MYSQL_DATABASE,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD
   }
 };
